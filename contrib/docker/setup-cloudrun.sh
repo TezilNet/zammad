a76ec47280574a4ec1c988226c4cdc46647f5ca4
Niji Saeba Media Noche <mathereall@gmail.com>
2024-10-08 11:37:14 -0600

    fix

--- contrib/docker/setup-cloudrun.sh
+++ contrib/docker/setup-cloudrun.sh
@@ -2,13 +2,16 @@
 set -e
 
 # Paquetes necesarios para la compilación y la configuración
-# PACKAGES="build-essential curl git libimlib2-dev libpq-dev shared-mime-info postgresql"
-PACKAGES="build-essential curl git libimlib2-dev libpq-dev shared-mime-info"
+PACKAGES="build-essential curl git libimlib2-dev libpq-dev shared-mime-info postgresql"
 
 # Actualizar e instalar dependencias necesarias
 apt-get update && \
 apt-get upgrade -y && \
 apt-get install -y --no-install-recommends ${PACKAGES}
+
+/etc/init.d/postgresql start
+su - postgres bash -c "createuser zammad -R -S"
+su - postgres bash -c "createdb --encoding=utf8 --owner=zammad zammad"
 
 # Cambiar al directorio de la aplicación
 cd "${ZAMMAD_DIR}"
@@ -21,14 +24,17 @@ bundle install
 
 # Precompilar los assets (sin necesidad de Redis)
 touch db/schema.rb
-ZAMMAD_SAFE_MODE=1 bundle exec rake assets:precompile
+ZAMMAD_SAFE_MODE=1 DATABASE_URL=postgresql://zammad:/zammad bundle exec rake assets:precompile
 
 # Limpiar archivos temporales y dependencias innecesarias
 # rm -r tmp/*
 # script/build/cleanup.sh
 
+/etc/init.d/postgresql stop
+
+
 # Limpiar listas de paquetes de apt para reducir el tamaño de la imagen
-apt-get remove -y build-essential git curl && \
+apt-get remove -y build-essential git curl postgresql && \
 apt-get autoremove -y && \
 apt-get clean
 # apt-get clean && \
